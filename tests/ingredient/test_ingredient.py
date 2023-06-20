83685af5927b769a43af3352a9d9cc6f838a804f
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-20 16:53:20 -0300

    feat: implementa testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient1_1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("tomate")
+    assert ingredient1.name == "queijo mussarela"
+    assert ingredient1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert ingredient1.__hash__() == ingredient1_1.__hash__()
+    assert ingredient1.__hash__() != ingredient2.__hash__()
+    assert ingredient1 == ingredient1_1
+    assert ingredient1 != ingredient2
+    assert ingredient1.__repr__() == "Ingredient('queijo mussarela')"
