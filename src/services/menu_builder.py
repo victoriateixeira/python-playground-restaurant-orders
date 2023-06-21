47826492b0fdbbf33259008ba09ce66c71f5104f
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-21 14:35:43 -0300

    feat: implementa parcialmente a classe MenuBuilder

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -11,6 +11,7 @@ class MenuBuilder:
     def __init__(self, data_path=DATA_PATH, inventory_path=INVENTORY_PATH):
         self.menu_data = MenuData(data_path)
         self.inventory = InventoryMapping(inventory_path)
+        print(self.menu_data.dishes)
 
     def make_order(self, dish_name: str) -> None:
         try:
@@ -26,4 +27,26 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        dishes_list = self.menu_data.dishes
+        # complete_menu = []
+        filtered_menu = []
+        for dish in dishes_list:
+            ingredients = dish.get_ingredients()
+            restrictions = dish.get_restrictions()
+            menu_item = {
+                "dish_name": dish.name,
+                "ingredients": ingredients,
+                "price": dish.price,
+                "restrictions": restrictions,
+            }
+            if restriction:
+                if restriction not in restrictions:
+                    filtered_menu.append(menu_item)
+
+            else:
+                filtered_menu.append(menu_item)
+        return filtered_menu
+
+
+menu = MenuBuilder()
+print(menu)
