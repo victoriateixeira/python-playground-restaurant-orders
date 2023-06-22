2d8948cbafdd0bdebfc642dd7c4e3fd78c667cdf
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-22 18:07:45 -0300

    feat: integra a feature de checcar o estoque classe MenuBuilder

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -11,7 +11,6 @@ class MenuBuilder:
     def __init__(self, data_path=DATA_PATH, inventory_path=INVENTORY_PATH):
         self.menu_data = MenuData(data_path)
         self.inventory = InventoryMapping(inventory_path)
-        print(self.menu_data.dishes)
 
     def make_order(self, dish_name: str) -> None:
         try:
@@ -33,20 +32,23 @@ class MenuBuilder:
         for dish in dishes_list:
             ingredients = dish.get_ingredients()
             restrictions = dish.get_restrictions()
-            menu_item = {
-                "dish_name": dish.name,
-                "ingredients": ingredients,
-                "price": dish.price,
-                "restrictions": restrictions,
-            }
-            if restriction:
-                if restriction not in restrictions:
-                    filtered_menu.append(menu_item)
+            recipe = dish.recipe
+            is_available = self.inventory.check_recipe_availability(recipe)
 
-            else:
-                filtered_menu.append(menu_item)
+            if is_available:
+                menu_item = {
+                    "dish_name": dish.name,
+                    "ingredients": ingredients,
+                    "price": dish.price,
+                    "restrictions": restrictions,
+                }
+                if restriction:
+                    if restriction not in restrictions:
+                        filtered_menu.append(menu_item)
+                else:
+                    filtered_menu.append(menu_item)
         return filtered_menu
 
 
-menu = MenuBuilder()
-print(menu)
+# menu = MenuBuilder()
+# print(menu.get_main_menu())
