f476daf3ecb35e8fae0f61b45594f2aef5f029d4
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-06-22 17:26:41 -0300

    feat: implementa classe InventoryMapping

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,17 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for key in recipe:
+            if key not in self.inventory or recipe[key] > self.inventory[key]:
+                return False
+
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        is_available = self.check_recipe_availability(recipe)
+        if not is_available:
+            raise ValueError
+
+        for key in recipe:
+            self.inventory[key] -= recipe[key]
